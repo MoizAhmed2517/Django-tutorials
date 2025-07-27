9385f39d05624aaab869b6cebb0de8de4c13c0e2
MoizGen1771 <moiz@generex.ai>
2025-07-27 21:33:54 +0500

    test debug

--- src/saas/settings.py
+++ src/saas/settings.py
@@ -7,7 +7,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-y$555mhe1yjnv%*u!d&@^=ij!3jkfx)(tl^l#5(o^*73us!_t8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     "django-tutorials-production.up.railway.app",
